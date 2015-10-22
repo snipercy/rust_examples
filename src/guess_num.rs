a95edf18bdfdef6b1379d33e71d96dc1cba882d4
531671430@qq.com <531671430@qq.com>
2015-10-22 21:48:21 +0800

    1

--- src/guess_num.rs
+++ src/guess_num.rs
@@ -1,17 +1,38 @@
 use std::io;
+use std::cmp::Ordering;
 
 fn main(){
     println!("Guess the number!");
-
-    println!("Please input your guess.");
-
-    // mut 表示变量 guess 可变
-    // rust变量默认是不可变
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line");
     
-    println!("You guessed {}", guess);
+    loop{
+         println!("Please input your guess.");
+
+        // mut 表示变量 guess 可变
+        // rust变量默认是不可变
+        let mut guess = String::new();
+
+        let secret_num= 5;
+
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
+     
+        // haha
+        // trim(),eliminate any white space
+        // parse(),parse a string into other a number
+        let guess: u32 = guess.trim().parse()
+            .ok()
+            .expect("Please type a num");
+       
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_num){
+            Ordering::Less     => println!("Small"),
+            Ordering::Greater  => println!("big"),
+            Ordering::Equal    => {
+                println!("You Win");
+                break;
+            }
+        }
+    }
 }
